35580f14f3e6ccfc3adb431e0531488e9351200f
lenniethelemming <lenniethelemming@gmail.com>
2012-03-30 14:20:31 +0000

    Experimental uplink working

--- cutdown/Code/OsirisPayload/RFM22_Wrapper.ino
+++ cutdown/Code/OsirisPayload/RFM22_Wrapper.ino
@@ -12,8 +12,8 @@
 */
 
 
-RF22::ModemConfig FSK_500bd=  { 0x2b, 0x03, 0xd0, 0xe0, 0x10, 0x62,0x00, 0x05, 0x40, 0x0a, 0x1d, 0x80, 0x60, 0x04, 0x19, 0x2c, 0x22,
-0x08 };// these values correspond to 500bps tx rate no manchestger FSK - and are the lowest values that are
+RF22::ModemConfig OOK_500bd=  { 0x2b, 0x03, 0xd0, 0xe0, 0x10, 0x62,0x00, 0x05, 0x40, 0x0a, 0x1d, 0x80, 0x60, 0x04, 0x19, 0x2c, 0x23,
+0x01 };// these values correspond to 500bps tx rate no manchestger OOK - and are the lowest values that are
 //stable on the RFM22b and give decent reception.
 
 
@@ -30,7 +30,7 @@ void RFM22B_RTTY_Mode(){
 
 void RFM22B_RX_Mode(){
 	rf22.setModeIdle(); // For some reason we need to go into an idle mode before switching to RX mode.
-	rf22.setModemRegisters(&FSK_500bd);
+	rf22.setModemRegisters(&OOK_500bd);
 	rf22.setModeRx(); 
 	rf22.setFrequency(RX_FREQ);
 }
