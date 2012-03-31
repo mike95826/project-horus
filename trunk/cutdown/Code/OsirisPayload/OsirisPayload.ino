37c0e784f8024545f3f29e5a2c4c0517a505b8ad
lenniethelemming@gmail.com <lenniethelemming@gmail.com@74b57789-fcf9-737c-90a4-01ed806f97cd>
2012-03-31 13:42:53 +0000

    Modified rtty string

--- trunk/cutdown/Code/OsirisPayload/OsirisPayload.ino
+++ trunk/cutdown/Code/OsirisPayload/OsirisPayload.ino
@@ -13,16 +13,19 @@
 
 #include <RF22.h>
 #include <SPI.h>
+#include <util/crc16.h>
 
 #define PWR_LED	A2
 #define	STATUS_LED	A3
 #define	ONEWIRE	3
 #define WIRE_FET 8
 #define VALVE_FET 7
+#define BATT	7
 
 #define TX_FREQ	431.650
 #define	RX_FREQ	431.650
 #define TX_POWER	RF22_TXPOW_1DBM  // Options are 1,2,5,8,11,14,17,20 dBm
+#define LISTEN_TIME	5000
 
 // Singleton instance of the RFM22B Library 
 RF22 rf22;
@@ -30,9 +33,12 @@ RF22 rf22;
 // Variables & Buffers
 char txbuffer [128];
 int	rfm_temp;
-uint8_t rssi_floor = 0;
-uint8_t got_msg = 0;
+int rssi_floor = 0;
+int last_rssi = 0;
 char relaymessage[40] = "No uplink received yet";
+int batt_mv = 0;
+unsigned int count = 0;
+unsigned int rx_count = 0;
 
 void setup(){
 	// Setup out IO pins
@@ -40,6 +46,7 @@ void setup(){
 	pinMode(STATUS_LED, OUTPUT);
 	pinMode(WIRE_FET, OUTPUT);
 	pinMode(VALVE_FET, OUTPUT);
+	analogReference(INTERNAL);
 	digitalWrite(PWR_LED, HIGH);
 	digitalWrite(STATUS_LED, LOW);
 	digitalWrite(WIRE_FET, LOW);
@@ -53,29 +60,36 @@ void setup(){
 }
 
 void loop(){
+	count++;
 	RFM22B_RTTY_Mode();
 	delay(100);
 	
 	rfm_temp = (rf22.temperatureRead( RF22_TSRANGE_M64_64C,0 ) / 2) - 64;
-	//rfm_temp = rfm_temp - 64;
+	batt_mv = analogRead(BATT) * 12;
 	
-	sprintf(txbuffer, "    VK5QI OSIRIS Payload. %d,%d,%d,%s\n", rfm_temp,rssi_floor, got_msg,relaymessage);
-	got_msg = 0;
+	sprintf(txbuffer, "$$$OSIRIS,VK5QI,%d,%d,%d,%d,%d,%s", count,rfm_temp,batt_mv,rssi_floor,last_rssi,relaymessage);
+	sprintf(txbuffer, "%s*%04X\n", txbuffer, gps_CRC16_checksum(txbuffer));
 	digitalWrite(STATUS_LED, LOW);
 	rtty_txstring(txbuffer);
 	delay(100);
+	
 	RFM22B_RX_Mode();
-	delay(500);
+	delay(400);
+	
 	digitalWrite(STATUS_LED, HIGH);
-	rssi_floor = rf22.rssiRead();
+	
+	rssi_floor = ((int)rf22.rssiRead()*51 - 12400)/100; // Linear approximation to the graph in the datasheet.
+	
 	uint8_t data[] = "READY FOR CMD";
     rf22.send(data, sizeof(data));
     rf22.waitPacketSent();
     digitalWrite(STATUS_LED, LOW);
+    
+    
 	uint8_t buf[RF22_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
-	if (rf22.waitAvailableTimeout(10000)){
-		got_msg = rf22.lastRssi();
+	if (rf22.waitAvailableTimeout(LISTEN_TIME)){
+		last_rssi = ((int)rf22.lastRssi()*51 - 12400)/100;
 		if(rf22.recv(buf, &len)){
 			for (int lc = 0; lc < 40; lc += 1){
               relaymessage[lc]=char(buf[lc]);  
@@ -83,24 +97,19 @@ void loop(){
         }
 		digitalWrite(STATUS_LED, HIGH);
 		delay(1000);
-		//uint8_t data[] = "OK";
-		//rf22.send(data, sizeof(data));
-    	//rf22.waitPacketSent();
-    	alert_sound();
+    	alert_sound(3);
 	}
 }
 
-void alert_sound(){
+void alert_sound(int loops){
 	RFM22B_RTTY_Mode();
 	delay(100);
-	rf22.spiWrite(0x073, 0x00);
-	delay(300);
-	rf22.spiWrite(0x073, 0x02);
-	delay(300);
-	rf22.spiWrite(0x073, 0x00);
-	delay(300);
-	rf22.spiWrite(0x073, 0x02);
-	delay(300);
+	for(int i = 0; i<loops; i++){
+		rf22.spiWrite(0x073, 0x00);
+		delay(250);
+		rf22.spiWrite(0x073, 0x02);
+		delay(250);
+	}
 }
 
 // Blink both LEDs simultaneously and fast, to indicate a failure.
@@ -113,4 +122,22 @@ void payload_fail(){
 		digitalWrite(STATUS_LED, LOW);
 		delay(300);
 	}
+}
+
+uint16_t gps_CRC16_checksum (char *string)
+{
+	size_t i;
+	uint16_t crc;
+	uint8_t c;
+ 
+	crc = 0xFFFF;
+ 
+	// Calculate checksum ignoring the first two $s
+	for (i = 2; i < strlen(string); i++)
+	{
+		c = string[i];
+		crc = _crc_xmodem_update (crc, c);
+	}
+ 
+	return crc;
 }
